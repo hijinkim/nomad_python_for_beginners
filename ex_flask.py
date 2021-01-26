d7966506c623ab4e126d6ad2fe0e03c7c8697b43
hijin <hijin1998@gmail.com>
2021-01-26 23:14:53 +0900

    update 0126

---
+++ ex_flask.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
+
+app = Flask("SuperScrapper")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        jobs = get_jobs(word)
+    else:
+        return redirect("/")
+    return render_template("report.html", searchingBy=word)
+
+
+app.run(host="localhost")
